2a0645f6c2a3fac1a58204173fcb8d87fd61f93a
shameermohamed <shameer50@gmail.com>
2022-03-18 17:41:52 +0530

    Create control-led-using-arduino-gsm-blynk.ino

---
+++ Control-LED-Using-Arduino-GSM-Blynk-IOT/control-led-using-arduino-gsm-blynk.ino
@@ -0,0 +1,73 @@
+//Tech Trends Shameer
+//Control LED Using Arduino + GSM + Blynk 2.0
+
+
+#define BLYNK_PRINT Serial
+
+/* Fill-in your Template ID (only if using Blynk.Cloud) */
+//#define BLYNK_TEMPLATE_ID   "YourTemplateID"
+#define BLYNK_TEMPLATE_ID "TemplateID"
+#define BLYNK_DEVICE_NAME "Arduino GSM Blynk LED"
+#define BLYNK_AUTH_TOKEN "AUTH Token"
+
+// Select your modem:
+#define TINY_GSM_MODEM_SIM800
+//#define TINY_GSM_MODEM_SIM900
+//#define TINY_GSM_MODEM_M590
+//#define TINY_GSM_MODEM_A6
+//#define TINY_GSM_MODEM_A7
+//#define TINY_GSM_MODEM_BG96
+//#define TINY_GSM_MODEM_XBEE
+
+// Default heartbeat interval for GSM is 60
+// If you want override this value, uncomment and set this option:
+//#define BLYNK_HEARTBEAT 30
+
+#include <TinyGsmClient.h>
+#include <BlynkSimpleTinyGSM.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = BLYNK_AUTH_TOKEN;
+
+// Your GPRS credentials
+// Leave empty, if missing user or pass
+char apn[]  = "internet";
+char user[] = "";
+char pass[] = "";
+
+// Hardware Serial on Mega, Leonardo, Micro
+//#define SerialAT Serial1
+
+// or Software Serial on Uno, Nano
+#include <SoftwareSerial.h>
+SoftwareSerial SerialAT(3, 2); // RX, TX
+
+TinyGsm modem(SerialAT);
+
+void setup()
+{
+  // Debug console
+  Serial.begin(9600);
+
+  delay(10);
+
+  // Set GSM module baud rate
+  SerialAT.begin(9600);
+  delay(3000);
+
+  // Restart takes quite some time
+  // To skip it, call init() instead of restart()
+  Serial.println("Initializing modem...");
+  modem.restart();
+
+  // Unlock your SIM card with a PIN
+  //modem.simUnlock("1234");
+
+  Blynk.begin(auth, modem, apn, user, pass);
+}
+
+void loop()
+{
+  Blynk.run();
+}
